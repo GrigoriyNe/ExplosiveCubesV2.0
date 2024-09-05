686df4a11d84a80ac76f238cc883addf93e8d2a9
GrigoriyNe <159809163+GrigoriyNe@users.noreply.github.com>
2024-09-05 22:01:39 +0500

    Update Exploder.cs

--- Assets/Scripts/Exploder.cs
+++ Assets/Scripts/Exploder.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _effect;
+
     public void Explode(Cube cube)
     {
         Collider[] colliders = Physics.OverlapSphere(cube.transform.position, cube.ExplosionRadius);
@@ -12,6 +14,7 @@ public class Exploder : MonoBehaviour
                 continue;
 
             rigidbody.AddExplosionForce(cube.ExplosionForce, cube.transform.position, cube.ExplosionRadius);
+            Instantiate(_effect, transform.position, transform.rotation);
         }
     }
 }
